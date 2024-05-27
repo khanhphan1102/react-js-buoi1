b96f6ee5ac24468ec3e9991eddb3cc3632d9bacd
khanhphan1102 <dongian98@gmail.com>
2024-05-27 16:46:03 +0700

    up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MainComponent from './MainComponent/MainComponent';
+
+function App() {
+  return (
+    <MainComponent/>
+  );
+}
+
+export default App;
